587aec36331731be7b20d656f8c46328d65883d8
AxelTLarsson <mail@axellarsson.nu>
2017-02-01 15:46:46 +0100

    Add run-local and run-cluster

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0.0-SNAPSHOT"
 scalaVersion := "2.10.6"
 
 // Change this to another test framework if you prefer
-// libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.3"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.3" % "provided"
 ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) }
 dependencyOverrides += "org.scala-lang" % "scala-compiler" % scalaVersion.value
 // Uncomment to use Akka
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -6,7 +6,7 @@ import org.apache.spark.SparkConf
 
 object Hello {
   def main(args: Array[String]) {
-    val logFile = "build.sbt" // Should be some file on your system
+    val logFile = args(0)// Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
     conf.setMaster("local[*]")
     val sc = new SparkContext(conf)
@@ -14,6 +14,7 @@ object Hello {
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    println("HEJ")
     sc.stop()
   }
 }
