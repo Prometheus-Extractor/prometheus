7f754b8c36786f5c29366ce6e2e17ca4b0181655
AxelTLarsson <mail@axellarsson.nu>
2017-02-01 11:39:16 +0100

    Experiments

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,18 @@
 package com.example
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object Hello {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+  def main(args: Array[String]) {
+    val logFile = "build.sbt" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application")
+    conf.setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
   }
 }
