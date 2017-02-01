a13b0a5968c26f39b0033f8a784ccb81fa9de4f8
Erik Gärtner <erik@gartner.io>
2017-02-01 12:52:19 +0100

    fix: Stop bug

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -8,11 +8,12 @@ object Hello {
   def main(args: Array[String]) {
     val logFile = "build.sbt" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application")
-    conf.setMaster("local[4]")
+    conf.setMaster("local[*]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    sc.stop()
   }
 }
