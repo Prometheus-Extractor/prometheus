43b4b696637641e66177a57afaf6d7929fee3299
Erik Gärtner <erik@gartner.io>
2017-02-16 14:40:12 +0100

    wip: Saving and loading training sentences

--- src/main/scala/com/sony/relationmodel/TrainingDataExtractor.scala
+++ src/main/scala/com/sony/relationmodel/TrainingDataExtractor.scala
@@ -1,11 +1,12 @@
 package com.sony.relationmodel
 
 import scala.collection.JavaConverters._
-
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
 import se.lth.cs.docforia.Document
 import se.lth.cs.docforia.graph.disambig.NamedEntityDisambiguation
 import se.lth.cs.docforia.graph.text.Sentence
+import se.lth.cs.docforia.memstore.MemoryDocumentIO
 import se.lth.cs.docforia.query.QueryCollectors
 
 /**
@@ -52,6 +53,26 @@ object TrainingDataExtractor {
     trainingData
   }
 
+  def load(path: String, sqlContext: SQLContext): Array[(Relation, RDD[TrainingSentence])] = {
+
+    import sqlContext.implicits._
+    val rawData = sqlContext.read.parquet(path).as[SerializedTrainingSentence].rdd
+    val a = rawData.map(st => {
+      (st.relation, TrainingSentence(MemoryDocumentIO.getInstance().fromBytes(st.sentenceDoc), st.entityPair))
+    }).groupByKey()
+
+  }
+
+  def save(data: Array[(Relation, RDD[TrainingSentence])], path: String, sqlContext: SQLContext): Unit = {
+
+    import sqlContext.implicits._
+    data.map(r => r._2.map(t => SerializedTrainingSentence(r._1, t.sentenceDoc.toBytes(), t.entityPair))).reduce((x, y) =>{
+      x ++ y
+    }).toDF().write.parquet(path)
+
+  }
+
 }
 
 case class TrainingSentence(sentenceDoc: Document, entityPair: EntityPair)
+private case class SerializedTrainingSentence(relation: Relation, sentenceDoc: Array[Byte], entityPair: EntityPair)
