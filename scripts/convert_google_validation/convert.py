6541b6021b03c2f8ea461732667ab1edb9cfb2af
Erik Gärtner <erik@gartner.io>
2017-03-20 14:00:55 +0100

    Add help text to convert script

--- scripts/convert_google_validation/convert.py
+++ scripts/convert_google_validation/convert.py
@@ -41,6 +41,10 @@ def convertItem(item, relations_id):
 
 if __name__ == '__main__':
 
+    if len(sys.argv) != 4:
+        print("convert.py infile outfile relation_id")
+        exit()
+
     relation_id = sys.argv[3]
     infile = open(sys.argv[1], 'r')
     outfile = open(sys.argv[2], 'w')
