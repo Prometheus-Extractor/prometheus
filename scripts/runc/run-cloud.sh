2b982f8e9849793f2f4f7aeca0e2862cd689decd
Erik Gärtner <erik@gartner.io>
2017-04-17 16:28:10 +0200

    Run cluster commands through screen

--- scripts/runc/run-cloud.sh
+++ scripts/runc/run-cloud.sh
@@ -64,14 +64,14 @@ execute rsync -av --delete -e ssh --progress target/pack/lib/ $REMOTE_HOST:$WORK
 execute scp target/scala-2.10/$JAR_NAME $REMOTE_HOST:$WORK_PATH/$JAR_NAME
 
 printf "$GREEN == Running $JAR_NAME on $REMOTE_HOST == $RES\n"
-execute ssh $REMOTE_HOST 'bash -s' << EOF
-  cd $WORK_PATH
-  $EXTRA_ENVS $SPARK_SUBMIT \
-    --conf spark.driver.maxResultSize=$SPARK_MAX_RESULTSIZE \
-    --conf spark.executor.extraJavaOptions="$JVMOPTS" \
-    --jars $LIBS \
-    $EXTRA_SPARK_OPTIONS \
-    $JAR_NAME $args
-EOF
+REMOTE_COMMAND="cd $WORK_PATH; "\
+"$EXTRA_ENVS $SPARK_SUBMIT "\
+"--conf spark.driver.maxResultSize=$SPARK_MAX_RESULTSIZE "\
+"--conf spark.executor.extraJavaOptions=\"$JVMOPTS\" "\
+"--jars $LIBS "\
+"$EXTRA_SPARK_OPTIONS "\
+"$JAR_NAME $args"
+SESSION_NAME="prometheus-"$(date +%R)
+execute ssh -t $REMOTE_HOST "screen -S $SESSION_NAME  bash -c '$REMOTE_COMMAND'"
 printf "$GREEN == Done == $RES\n"
 
